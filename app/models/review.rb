e951e86e6d3fbd2d6e2970438737e618907ebc6f
Mit Donga <mit@complitech.net>
2022-08-17 22:27:20 +0530

    SEO friendly URLs

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,9 @@ class Review < ApplicationRecord
    validates :title, presence: true, length: { minimum: 10 }
    validates :body, presence: true, length: { minimum: 20 }
    validates :score, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 10 }
+
+   def to_param
+      id.to_s + "-" + title.parameterize
+   end
+
 end
