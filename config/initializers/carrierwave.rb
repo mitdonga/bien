ff453d152c3517ac237394b75606571e1117c584
mit-complitech <mit@complitech.com>
2022-09-06 22:32:10 +0530

    Added aws_bucket name

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -17,7 +17,7 @@
 
 CarrierWave.configure do |config|
   config.storage    = :aws
-  config.aws_bucket = ENV.fetch('S3_BUCKET_NAME')
+  config.aws_bucket = 'bien02'
   config.aws_acl    = 'public-read'
   config.aws_authenticated_url_expiration = 60 * 60 * 24 * 7
   config.aws_credentials = {
